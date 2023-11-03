f19463868a1bab28a1a8d0e8d642a83280f0bce4
12fatime <ldgq8cjw@code.edu.az>
2023-11-03 08:39:01 +0400

    jstask

---
+++ script.js
@@ -0,0 +1,29 @@
+let chekiNihad=78;
+let boyNihad=169;
+
+let chekiNail=92;
+let boyNail=195;
+
+let bkiNihad=chekiNihad / (  boyNihad**2);
+let bkiNail=chekiNail / (boyNail**2);
+
+if (bkiNihad>bkiNail){
+    console.log(bkiNihad>bkiNail)
+} else {
+    console.log(bkiNihad>bkiNail)
+}
+
+let cheki2Nihad=95;
+let boy2Nihad=188;
+
+let cheki2Nail=85;
+let boy2Nail=176;
+
+let bki2Nihad=cheki2Nihad/( boy2Nihad**2);
+let bki2Nail=cheki2Nail/( boy2Nail**2);
+
+if (bki2Nihad>bki2Nail){
+    console.log(bki2Nihad>bki2Nail)
+}else{
+    console.log(bki2Nihad>bki2Nail)
+}
